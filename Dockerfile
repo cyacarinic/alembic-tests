ab5e04282e45edb8015b31f7951ac5a691435a14
Andres Chavez Ulfe <achavez@Andres-Chavez.local>
2016-07-26 15:11:23 -0500

    Adds

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ MAINTAINER Andres Chavez <achavez@rcp.pe>
 ADD ./app /srv/www/apidemo
 WORKDIR /srv/www/apidemo
 
+RUN apt-get update
+RUN apt-get -y install python-dev python-psycopg2
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
---
+++ app/api/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    guid = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+    def __repr__(self):
+        return '<User %d|%s>' % (self.id, self.email)

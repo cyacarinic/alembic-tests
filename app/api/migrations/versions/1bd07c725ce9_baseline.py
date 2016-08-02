ea498c5cd34ed3352a17f2320fcd094391dd90a5
Claudio Yacarini <cyacarini@rcp.pe>
2016-08-01 21:52:47 -0500

    Fix migrations

---
+++ app/api/migrations/versions/1bd07c725ce9_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 1bd07c725ce9
+Revises: 
+Create Date: 2016-08-01 22:56:34.433899
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1bd07c725ce9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
